45dfc12b11e9b0991174dc2443e64dcd7d6cdfe0
Glitch (metazoabot) <none>
2023-12-25 03:50:56 +0000

    🏛🍽 Checkpoint ./server.js:63694031/281:63707189/8

--- server.js
+++ server.js
@@ -38,22 +38,26 @@ const commands = [
     name: "ping",
     description: "Replies with Pong!",
   },
+  {
+    name: "pregnant",
+    description: "Replies with 子宮なでなでしたい",
+  },
 ];
 
-// const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN);
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN);
 
-// (async () => {
-//   try {
-//     console.log('Started refreshing application (/) commands.');
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
 
-//     // ここで実際に設定したコマンドを登録している
-//     await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+    // ここで実際に設定したコマンドを登録している
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
 
-//     console.log('Successfully reloaded application (/) commands.');
-//   } catch (error) {
-//     console.error(error);
-//   }
-// })();
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
 
 /*
  * BOT起動
@@ -67,6 +71,9 @@ client.on("interactionCreate", async (interaction) => {
 
   if (interaction.commandName === "ping") {
     await interaction.reply("Pong!");
+  }
+  if (interaction.commandName === "pregnant") {
+    await interaction.reply("子宮なでなでしたい");
   }
 });
 
